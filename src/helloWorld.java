1ac1695ad011713d23959ea66ec17122fc198095
sanskarOH <sanskaryeeha@gmail.com>
2026-03-18 13:30:18 +0530

    testing the build

--- src/helloWorld.java
+++ src/helloWorld.java
@@ -1,5 +1,5 @@
 public class helloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins CI Pipeline!");
+        System.out.println("Hello testing the build!");
     }
 }
